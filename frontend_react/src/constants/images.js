d568f85055f97fecab4a9c11b4d644fea5cf4cd9
anubhav0821 <124450638+anubhav0821@users.noreply.github.com>
2023-04-24 08:03:50 +0000

    Change in the logo in navbar

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -26,7 +26,7 @@ import about04 from '../assets/about04.png';
 import profile from '../assets/profile.png';
 import portfolio from '../assets/portfolio.png';
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpeg';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
